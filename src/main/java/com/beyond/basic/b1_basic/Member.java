acf39c5ee4b04ff8518bb22ff52e662f2dd34aa9
2001056 <lbchan1027@gmail.com>
2026-01-14 16:58:32 +0900

    a

---
+++ src/main/java/com/beyond/basic/b1_basic/Member.java
@@ -0,0 +1,14 @@
+package com.beyond.basic.b1_basic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    private String name;
+    private String email;
+
+}
